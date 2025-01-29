dec55e21a3fae2188ab7a7ba7a4d2b31cc54af4c
hii-sayan <67260880+hii-sayan@users.noreply.github.com>
2025-01-29 09:31:19 +0530

    Update contracts.ts

--- frontend/src/constants/contracts.ts
+++ frontend/src/constants/contracts.ts
@@ -1,6 +1,6 @@
 // frontend/src/constants/contracts.ts
-import TokenArtifact from '/Users/sayan/Desktop/x1_bkend/artifacts/contracts/X1Coin.sol/X1Coin.json';
-import StakingArtifact from '/Users/sayan/Desktop/x1_bkend/artifacts/contracts/StakingContract.sol/StakingContract.json';
+import TokenArtifact from '../../../artifacts/contracts/X1Coin.sol/X1Coin.json';
+import StakingArtifact from '../../../artifacts/contracts/StakingContract.sol/StakingContract.json';
 
 export const CONTRACTS = {
   Token: {
